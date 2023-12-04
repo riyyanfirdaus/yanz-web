dab4b8b2ad766d1a898123d74ac45db3d01aa495
riyyanfir <m.riyyan.firdaus@gmail.com>
2023-12-04 12:25:39 +0700

    feat: add image domain configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/devrbr7ot/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
