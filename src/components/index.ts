a5476d43825747f2aa4eeda10de4f4eec31adc6c
riyyanfir <m.riyyan.firdaus@gmail.com>
2023-12-04 12:42:06 +0700

    feat: add all components

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import ProjectsSection from "./ProjectsSection";
+import DarkModeToggle from "./DarkModeToggle";
+import ThemeProvider from "./ThemeProvider";
+import AboutSection from "./AboutSection";
+import HeroSection from "./HeroSection";
+import EmailSection from "./EmailSection";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export { ThemeProvider, DarkModeToggle, ProjectsSection, AboutSection, HeroSection, EmailSection, Navbar, Footer };
